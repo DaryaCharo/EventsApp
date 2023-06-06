9d7340c8e3cfe94304a7f32f0d4422d16e7b83da
DaryaCharo <daryacharo@gmail.com>
2023-06-06 11:11:34 +0300

    Initial Commit

---
+++ EventsApp/EventsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsAppApp.swift
+//  EventsApp
+//
+//  Created by Darya Charniankova on 6.06.23.
+//
+
+import SwiftUI
+
+@main
+struct EventsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
